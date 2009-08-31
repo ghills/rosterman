2b9dcee976f5d11f422815af5a35ae527db21312
ghills <ghills@gmail.com>
2009-08-31 08:26:54 -0700

    Building initial form entry for new players

--- roster/urls.py
+++ roster/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('rosterman.roster.views',
     # Example:
     (r'^$', 'index'),
     (r'^(?P<team_id>\d+)/$', 'detail'),
+    (r'^(?P<team_id>\d+)/add_player/$', 'add_player'),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
--- roster/views.py
+++ roster/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from roster.models import Team
+from roster.models import Team, Player
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
@@ -10,3 +12,19 @@ def index(request):
 def detail(request,team_id):
     t = get_object_or_404(Team, pk=team_id)
     return render_to_response('roster/detail.html', {'team': t})
+    
+def add_player(request,team_id):
+    t = get_object_or_404(Team,pk=team_id)
+    try:
+        p = Player()
+        p.first_name = request.POST['first_name']
+        p.last_name = request.POST['last_name']
+        p.number = request.POST['number']
+        p.height = request.POST['height']
+    except (KeyError):
+        return render_to_response("roster/%d" % team_id, {
+                                   'error_message' : "You forgot fields"})
+    else:
+        p.team = t
+        p.save()
+        return HttpResponseRedirect(reverse('roster',args=(team_id)))
