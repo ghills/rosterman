7fed7bc4c26e584e39e8abbe1c6d621e1ee86753
ghills <ghills@gmail.com>
2009-08-31 15:16:31 -0700

    Admin interface for current models

---
+++ roster/admin.py
@@ -0,0 +1,9 @@
+from rosterman.roster.models import City
+from rosterman.roster.models import Team
+from rosterman.roster.models import Player
+
+from django.contrib import admin
+
+admin.site.register(City)
+admin.site.register(Team)
+admin.site.register(Player)
